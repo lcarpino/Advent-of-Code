60ab6f57b5b5ab7403dd2aeb7cc7a338df9b0907
Luke Arpino <luke.c.arpino@gmail.com>
2018-12-08 01:17:52 +0000

    Add: solution to day 4
    Serious refactor needed.

--- src/04.py
+++ src/04.py
@@ -1,13 +1,21 @@
 #! /usr/bin/env python3
 
 from pathlib import Path
-from collections import namedtuple
+from itertools import groupby
+from collections import Counter
 import re
 
-def parse(lines):
-    for line in sorted(lines):
-        (time, message) = re.match(r'\[(.*)\] (.*)', line).groups()
-        print(time, message)
+# is it worth using the datetime module instead to do this?
+def parse(unsorted_records):
+    for line in sorted(unsorted_records):
+        (datetime, message) = re.match(r'\[(.*)\] (.*)', line).groups()
+        if   message.startswith('Guard'):
+            guard = int(message.split()[1][1:])
+        elif message == 'falls asleep':
+            sleep_time = int(datetime[-2:])
+        elif message == 'wakes up':
+            wake_time = int(datetime[-2:])
+            yield (guard, sleep_time, wake_time)
 
 def part1():
     pass
@@ -18,5 +26,22 @@ def part2():
 if __name__ == '__main__':
 
     # print(part1())
-     with open(str(Path('../data/04.dat')), 'r') as f:
-         parse(f)
+    with open(str(Path('../data/04.dat')), 'r') as f:
+        data = parse(f)
+
+        # print(sorted(data, key=lambda gsw: gsw[0]))
+
+        test = {k: [minute for (_, sleep_time, wake_time) in g
+                    for minute in range(sleep_time, wake_time)]
+                for (k, g) in groupby(sorted(data, key=lambda gsw: gsw[0]), lambda gsw: gsw[0])}
+
+        guard, minutes = max(test.items(), key=lambda gt: len(gt[1]))
+
+        print(guard) #863
+        print(Counter(minutes).most_common(1)) #46
+
+        times = {k: Counter(minutes).most_common(1)[0] for (k, minutes) in test.items()}
+        guard = max(times, key=lambda k: times[k][1])
+
+        print(guard) #373
+        print(times[guard]) #40
