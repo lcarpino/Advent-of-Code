c5240022a1e204294e178c02126f809645f8d0ab
Luke Arpino <luke.c.arpino@gmail.com>
2018-12-09 17:09:27 +0000

    Add: solutions to day 5

--- src/05.py
+++ src/05.py
@@ -1,12 +1,35 @@
 #! /usr/bin/env python3
 
 from pathlib import Path
+from functools import reduce
 
-def part1():
-    pass
+def parse(polymer):
+    for line in polymer:
+        return line.strip()
 
-def part2():
-    pass
+def polymer_reducer(base_chain, new_base):
+    # if   base_chain and base_chain[-1] != new_base and base_chain[-1].upper() == new_base.upper():
+    if   base_chain and base_chain[-1] == new_base.swapcase():
+        base = base_chain[:-1]
+    else:
+        base = base_chain + new_base
+    return base
+
+def polymerase(polymer):
+    return reduce(polymer_reducer, polymer)
+
+def part1(polymer):
+    return len(polymerase(polymer))
+
+def part2(polymer):
+    return min(
+        (len(polymerase(polymer.replace(base, '').replace(base.upper(), '')))
+                for base in 'abcdefghijklmnopqrstuvwxyz'))
 
 if __name__ == '__main__':
-    pass
+
+    with open(str(Path('../data/05.dat')), 'r') as f:
+        data = parse(f)
+
+        print(part1(data))
+        print(part2(data))
